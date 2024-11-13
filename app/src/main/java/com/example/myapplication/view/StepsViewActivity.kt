3247a515e012b333836740b2811a42c8ce9b623d
chenyongda2018 <chenchenyongda@gmail.com>
2024-11-13 15:35:43 +0800

    [update]GradientDrawable

--- app/src/main/java/com/example/myapplication/view/StepsViewActivity.kt
+++ app/src/main/java/com/example/myapplication/view/StepsViewActivity.kt
@@ -1,18 +1,43 @@
 package com.example.myapplication.view
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.LiveData
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityStepsViewBinding
+import com.example.simplekit.utils.dp
+import com.example.simplekit.widget.RoundRectDrawable
 
 class StepsViewActivity : AppCompatActivity() {
 
     private val mBinding by lazy {
         ActivityStepsViewBinding.inflate(layoutInflater)
     }
+
+    private val mSignedBg by lazy {
+        GradientDrawable().apply {
+            this.shape = GradientDrawable.RECTANGLE
+            this.cornerRadius = 12.dp.toFloat()
+            this.setStroke(1.dp,Color.parseColor("#9CD7FF"))
+            this.setColor(Color.parseColor("#294B7A"))
+        }
+    }
+
+    private val mUnSignedBg by lazy {
+        GradientDrawable().apply {
+            this.shape = GradientDrawable.RECTANGLE
+            this.cornerRadius = 12.dp.toFloat()
+            this.setStroke(1.dp,Color.parseColor("#9CD7FF"))
+            this.setColor(Color.parseColor("#294B7A"))
+        }
+    }
+
+    private var mSignedState = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +60,27 @@ class StepsViewActivity : AppCompatActivity() {
         mBinding.minusBtn.setOnClickListener {
             curProgress-=5
             mBinding.progress.setProgress(curProgress)
+        }
+
+        mBinding.roundBtn.background = GradientDrawable().apply {
+            this.shape = GradientDrawable.RECTANGLE
+            this.cornerRadius = 12.dp.toFloat()
+            this.setStroke(2.dp,Color.parseColor("#9CD7FF"))
+            this.setColor(Color.parseColor("#294B7A"))
+        }
+
+        onSignStateChange()
+        mBinding.roundBtn.setOnClickListener {
+            mSignedState = !mSignedState
+            onSignStateChange()
+        }
+    }
+
+    private fun onSignStateChange() {
+        if(mSignedState) {
+            mBinding.roundBtn.alpha = 1f
+        } else {
+            mBinding.roundBtn.alpha = 0.5f
         }
     }
 }
